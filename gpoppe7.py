30c4071bb155776ad771e007aca89ed63f78a32d
GP <gpoppe@csudh.edu>
2026-02-25 01:44:24 +0000

    lab 7

---
+++ gpoppe7.py
@@ -0,0 +1,14 @@
+#Garrett
+#lab #7
+
+
+x = 5
+
+if x > 6:
+    print("X is greater than 6")
+elif x < 6:
+    print("X is less than 6 and the value of x is ",x)
+else:
+    print("x is ",x," which is equal to 6")
+
+
