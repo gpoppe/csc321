e1a5668ee820c9598bc294e74e4209ca3ff40da5
GP <gpoppe@csudh.edu>
2026-03-04 02:05:59 +0000

    lab 8 example

---
+++ gpoppe8.py
@@ -0,0 +1,23 @@
+#Garrett Poppe
+
+userinput = input("enter your name:")
+
+print("Your name is ",userinput)
+x = 0
+z = int(userinput)
+x = 5 + z
+
+print("x = ",x)
+
+for i in range(5):
+    print(i)
+
+while (x > 0):
+    print(" X = ",x)
+    x = x -1
+
+
+
+
+
+
